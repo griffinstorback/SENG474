f26ace11f13d410f76a04f3a67212bf91addb660
Griffin Storback <griffinstorback@w134-87-148-014.wireless.uvic.ca>
2019-03-23 11:56:44 -0700

    progress

--- final_project/optimal_shot_location.py
+++ final_project/optimal_shot_location.py
@@ -3,8 +3,23 @@ import sys
 import time
 import csv
 
-# project done with three group members,
+# project done in group of three,
 # Griffin Storback - V00849885
 # Jeffrey Olmstead - V00852585
-# 
+# Dallas Brooks - V00
 
+t0 = time.time()
+
+if (len(sys.argv) != 2):
+    print("Wrong number of parameters.")
+    print("File should be run with one parameter; the name of the file")
+    exit()
+
+f = open(sys.argv[1])
+
+for line in f:
+    line_values = line.rsplit(",")
+    event = line_values[5]
+    secondary_type = line_values[6]
+    x = line_values[7]
+    y = line_values[8]
